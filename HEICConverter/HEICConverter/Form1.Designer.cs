c94f8c83784bc8e190e6c56393df77ab7692c144
HungVV6574 <HungVV6574@seta-asia.com.vn>
2018-05-18 16:25:32 +0700

    HEIC Demo

---
+++ HEICConverter/HEICConverter/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace HEICConverter
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxView = new System.Windows.Forms.PictureBox();
+            this.btnChoose = new System.Windows.Forms.Button();
+            this.btnDownload = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxView
+            // 
+            this.pictureBoxView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxView.BackColor = System.Drawing.SystemColors.Info;
+            this.pictureBoxView.Location = new System.Drawing.Point(12, 52);
+            this.pictureBoxView.Name = "pictureBoxView";
+            this.pictureBoxView.Size = new System.Drawing.Size(888, 514);
+            this.pictureBoxView.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxView.TabIndex = 0;
+            this.pictureBoxView.TabStop = false;
+            // 
+            // btnChoose
+            // 
+            this.btnChoose.Location = new System.Drawing.Point(13, 13);
+            this.btnChoose.Name = "btnChoose";
+            this.btnChoose.Size = new System.Drawing.Size(122, 33);
+            this.btnChoose.TabIndex = 1;
+            this.btnChoose.Text = "Choose .HEIC";
+            this.btnChoose.UseVisualStyleBackColor = true;
+            this.btnChoose.Click += new System.EventHandler(this.btnChoose_Click);
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(141, 13);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(122, 33);
+            this.btnDownload.TabIndex = 1;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(912, 578);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.btnChoose);
+            this.Controls.Add(this.pictureBoxView);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxView;
+        private System.Windows.Forms.Button btnChoose;
+        private System.Windows.Forms.Button btnDownload;
+    }
+}
+
---
+++ HEICConverter/HEICConverter/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HEICConverter
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            HEICInvoke.read_profile();
+        }
+
+        private void btnChoose_Click(object sender, EventArgs e)
+        {
+            var filepicker = new OpenFileDialog();
+            filepicker.Title = "Chọn file";
+            filepicker.Multiselect = false;
+            filepicker.Filter = "HEIF(.heic)|*.heic";
+            if (filepicker.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            var tempheicfile = HEICInvoke.read_heif(filepicker.FileName);
+            int copysize = 0;
+            pictureBoxView.Image = HEICInvoke.ImageFromByte(HEICInvoke.invoke_heif2jpg(tempheicfile, 50, "tmp/temp_bitstream.hevc", ref copysize, false, false));
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.DefaultExt = "jpeg";
+            dialog.Filter = "*.jpg, *.jpeg|*.*";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBoxView.Image.Save(dialog.FileName, ImageFormat.Jpeg);
+            }
+        }
+    }
+}
